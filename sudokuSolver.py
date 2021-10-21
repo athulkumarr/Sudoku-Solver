97b91b7fdf6f3b3344e9ebf2b9a88e45cf2bfd2c
AthulKumar R <36944209+athulkumarr@users.noreply.github.com>
2021-10-21 20:37:55 +0530

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,4 +1,51 @@
-public void solveSudoku(char[][] board) 
-{
-        
-}
+def solveSudoku(self, board: List[List[str]]) -> None:
+    def get_block(i, j):
+        return 3*(i//3)+j//3
+
+    rows, cols, blocks = [set() for _ in range(9)], [set() for _ in range(9)], [set() for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            n = board[i][j]
+            rows[i].add(n)
+            cols[j].add(n)
+            blocks[get_block(i,j)].add(n)
+
+    def next_empty_cell(i,j):
+        j += 1
+        if j == 9:
+            j = 0
+            i += 1
+        if i == 9: return None
+        if board[i][j] != '.':
+            return next_empty_cell(i, j)
+        else:
+            return i, j
+
+    def fill_cell(i,j):
+        k = get_block(i,j)
+        for n in range(1, 10):
+            n = str(n)
+            if n not in cols[j] and n not in rows[i] and n not in blocks[k]:
+                board[i][j] = n
+                rows[i].add(n)
+                cols[j].add(n)
+                blocks[k].add(n)
+
+                end = next_empty_cell(i,j)
+                if end == None: return True
+
+                ni, nj = end
+
+                # fill the next cell by backtracking
+                if fill_cell(ni, nj): return True
+
+                # this digit did not work, remove it from the cell
+                board[i][j] = '.'
+                rows[i].remove(n)
+                cols[j].remove(n)
+                blocks[k].remove(n)
+
+        return False
+    i,j = next_empty_cell(0,-1)
+    fill_cell(i, j)
